9f8aa5ca18c17675a24c713c667a031be64ff5f7
jesperi.kuula <jeppe.kuula@gmail.com>
2023-02-20 16:54:26 +0200

    feat: Advanced query models

---
+++ Chinook/Models/CustomerCountry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Models
+{
+    public readonly record struct CustomerCountry(string Country, int TotalCustomers)
+    {
+    }
+}
---
+++ Chinook/Models/CustomerSpend.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Models
+{
+    public readonly record struct CustomerSpend(int CustomerId, double TotalSpend)
+    {
+    }
+}
